4a56856401d25a4dd07d67a58e1d6368a87653e6
manu <manu@manu-linux.(none)>
2013-12-15 22:40:02 +0100

    adding dropdown, EnsembleQuestions, TextArea and Text to M2T

--- EXJ2/src/transformation/TransfoModelToText.xtend
+++ EXJ2/src/transformation/TransfoModelToText.xtend
@@ -10,6 +10,11 @@ import org.eclipse.emf.ecore.resource.ResourceSet
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
 import mmui.MetaLayout
 import mmui.Radio
+import mmui.DropDown
+import mmui.Text
+import mmui.EnsembleQuestions
+import org.eclipse.emf.common.util.EList
+import mmui.TextArea
 
 class TransfoModelToText {
 	def MetaLayout loadXMI(String fileName)
@@ -25,9 +30,16 @@ class TransfoModelToText {
 		var metaLay = loadXMI(fileNameXMI)
 		var template = '''
 			<html>
+			<head>
+				<meta charset="UTF-8">
+				<title>Sondage généré</title>
+				<link rel='stylesheet' href='style.css' type='text/css' media='all' />
+			</head>
+			<body>
 			«
 				parcoursMetaLayout(metaLay)
 			»
+			</body>
 			</html>
 		'''
 		storeToFile(fileNameOut, template)
@@ -59,27 +71,49 @@ class TransfoModelToText {
 		var tmp = "";
 		while(el != null)
 		{
-			if(el instanceof CheckBox)
-				tmp = tmp + rendererCheckbox(el as CheckBox)
-			else if(el instanceof Radio)
-				tmp = tmp + rendererRadio(el as Radio)
+			tmp = tmp + switchElementUI(el)
 			el = el.next
 		}
 		return tmp
 	}
 	
+	def String switchElementUI(ElementUI el)
+	{
+		if(el instanceof CheckBox)
+			return(rendererCheckbox(el as CheckBox))
+		else if(el instanceof Radio)
+			return(rendererRadio(el as Radio))
+		else if(el instanceof DropDown)
+			return(rendererDropDown(el as DropDown))
+		else if(el instanceof Text)
+			return(rendererText(el as Text))
+		else if(el instanceof TextArea)
+			return(rendererTextArea(el as TextArea))
+		else if(el instanceof EnsembleQuestions)
+			return(rendererEnsembleQuestions(el as EnsembleQuestions))
+		else return('''<p>Erreur de type sur «el.question»''')
+	}
+	
 	def rendererCheckbox(CheckBox chk)
 	{
-		var tmp = '''
-			<label «IF chk.id != null» id="«chk.id»«ENDIF»">
-				«chk.question»
-			</label>
-		'''
-		val itChk = chk.options.iterator
-		while(itChk.hasNext())
+		var tmp = ''
+		if(chk.options.length > 0)
 		{
-			val opt = itChk.next()
-			tmp = tmp + '''<input type="checkbox" name="«opt.value»" value="1" />«opt.value»<br>'''
+			tmp = '''
+				<label «IF chk.id != null» id="«chk.id»«ENDIF»">
+					«chk.question»
+				</label>
+			'''
+			val itChk = chk.options.iterator
+			while(itChk.hasNext())
+			{
+				val opt = itChk.next()
+				tmp = tmp + '''<input type="checkbox" name="«opt.value»" value="1" />«opt.value»<br>'''
+			}
+		}
+		else
+		{
+			tmp = '''<input type="checkbox" name="«IF chk.id != null»«chk.id»«ELSE»«chk.question»«ENDIF»" value="1" />«chk.question»'''
 		}
 		return tmp + "<br>"
 	}
@@ -99,6 +133,71 @@ class TransfoModelToText {
 			tmp = tmp + '''<input type="radio" name="«rd.id»" value="«id»" «IF id == 0»checked «ENDIF»/>«opt.value» '''
 			id = id + 1
 		}
+		return tmp + "<br><br>"
+	}
+	
+	def rendererDropDown(DropDown dd)
+	{
+		var tmp = '''
+			<label «IF dd.id != null» id="«dd.id»«ENDIF»">
+				«dd.question»
+			</label>
+			<select name="«IF dd.id != null»«dd.id»«ELSE»«dd.question»«ENDIF»">
+		'''
+		val itRd = dd.options.iterator
+		var id = 0
+		while(itRd.hasNext())
+		{
+			val opt = itRd.next()
+			tmp = tmp + '''<option value="«id»"/>«opt.value»</option>
+			'''
+			id = id + 1
+		}
+		tmp = tmp + '''</select>'''
+		return tmp + "<br><br>"
+	}
+	
+	def rendererText(Text txt)
+	{
+		var tmp = '''
+			<label «IF txt.id != null» id="«txt.id»«ENDIF»">
+				«txt.question»
+			</label>
+			<input type="text" name="«IF txt.id != null»«txt.id»«ELSE»«txt.question»«ENDIF»" />
+		'''
+		return tmp + "<br><br>"
+	}
+	
+	def rendererTextArea(TextArea txt)
+	{
+		var tmp = '''
+			<label «IF txt.id != null» id="«txt.id»«ENDIF»">
+				«txt.question»
+			</label>
+			<textarea rows="4" cols="50" name="«IF txt.id != null»«txt.id»«ELSE»«txt.question»«ENDIF»"></textarea>
+		'''
+		return tmp + "<br><br>"
+	}
+	
+	def parcoursSousQuestions(EList<ElementUI> listeSousQuestion)
+	{
+		var tmp = "";
+		var itL = listeSousQuestion.iterator
+		while(itL.hasNext())
+		{
+			tmp = tmp + switchElementUI(itL.next())
+		}
+		return tmp
+	}
+	
+	def rendererEnsembleQuestions(EnsembleQuestions eq)
+	{
+		var tmp = '''
+			<fieldset>
+				<legend>«eq.question»</legend>
+					«parcoursSousQuestions(eq.listeSousQuestion)»
+			</fieldset>
+		'''
 		return tmp + "<br><br>"
 	}
 	
--- EXJ2/xtend-gen/transformation/TransfoModelToText.java
+++ EXJ2/xtend-gen/transformation/TransfoModelToText.java
@@ -5,11 +5,15 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import mmui.CheckBox;
+import mmui.DropDown;
 import mmui.ElementUI;
+import mmui.EnsembleQuestions;
 import mmui.Layout;
 import mmui.MetaLayout;
 import mmui.Option;
 import mmui.Radio;
+import mmui.Text;
+import mmui.TextArea;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -17,6 +21,7 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
@@ -41,9 +46,26 @@ public class TransfoModelToText {
       StringConcatenation _builder = new StringConcatenation();
       _builder.append("<html>");
       _builder.newLine();
+      _builder.append("<head>");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("<meta charset=\"UTF-8\">");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("<title>Sondage généré</title>");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("<link rel=\'stylesheet\' href=\'style.css\' type=\'text/css\' media=\'all\' />");
+      _builder.newLine();
+      _builder.append("</head>");
+      _builder.newLine();
+      _builder.append("<body>");
+      _builder.newLine();
       String _parcoursMetaLayout = this.parcoursMetaLayout(metaLay);
       _builder.append(_parcoursMetaLayout, "");
       _builder.newLineIfNotEmpty();
+      _builder.append("</body>");
+      _builder.newLine();
       _builder.append("</html>");
       _builder.newLine();
       String template = _builder.toString();
@@ -92,17 +114,9 @@ public class TransfoModelToText {
     boolean _while = _notEquals;
     while (_while) {
       {
-        if ((el instanceof CheckBox)) {
-          String _rendererCheckbox = this.rendererCheckbox(((CheckBox) el));
-          String _plus = (tmp + _rendererCheckbox);
-          tmp = _plus;
-        } else {
-          if ((el instanceof Radio)) {
-            String _rendererRadio = this.rendererRadio(((Radio) el));
-            String _plus_1 = (tmp + _rendererRadio);
-            tmp = _plus_1;
-          }
-        }
+        String _switchElementUI = this.switchElementUI(el);
+        String _plus = (tmp + _switchElementUI);
+        tmp = _plus;
         ElementUI _next = el.getNext();
         el = _next;
       }
@@ -112,47 +126,103 @@ public class TransfoModelToText {
     return tmp;
   }
   
-  public String rendererCheckbox(final CheckBox chk) {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("<label ");
-    {
-      String _id = chk.getId();
-      boolean _notEquals = (!Objects.equal(_id, null));
-      if (_notEquals) {
-        _builder.append(" id=\"");
-        String _id_1 = chk.getId();
-        _builder.append(_id_1, "");
+  public String switchElementUI(final ElementUI el) {
+    if ((el instanceof CheckBox)) {
+      return this.rendererCheckbox(((CheckBox) el));
+    } else {
+      if ((el instanceof Radio)) {
+        return this.rendererRadio(((Radio) el));
+      } else {
+        if ((el instanceof DropDown)) {
+          return this.rendererDropDown(((DropDown) el));
+        } else {
+          if ((el instanceof Text)) {
+            return this.rendererText(((Text) el));
+          } else {
+            if ((el instanceof TextArea)) {
+              return this.rendererTextArea(((TextArea) el));
+            } else {
+              if ((el instanceof EnsembleQuestions)) {
+                return this.rendererEnsembleQuestions(((EnsembleQuestions) el));
+              } else {
+                StringConcatenation _builder = new StringConcatenation();
+                _builder.append("<p>Erreur de type sur ");
+                String _question = el.getQuestion();
+                _builder.append(_question, "");
+                return _builder.toString();
+              }
+            }
+          }
+        }
       }
     }
-    _builder.append("\">");
-    _builder.newLineIfNotEmpty();
-    _builder.append("\t");
-    String _question = chk.getQuestion();
-    _builder.append(_question, "	");
-    _builder.newLineIfNotEmpty();
-    _builder.append("</label>");
-    _builder.newLine();
-    String tmp = _builder.toString();
+  }
+  
+  public String rendererCheckbox(final CheckBox chk) {
+    String tmp = "";
     EList<Option> _options = chk.getOptions();
-    final Iterator<Option> itChk = _options.iterator();
-    boolean _hasNext = itChk.hasNext();
-    boolean _while = _hasNext;
-    while (_while) {
+    int _length = ((Object[])Conversions.unwrapArray(_options, Object.class)).length;
+    boolean _greaterThan = (_length > 0);
+    if (_greaterThan) {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("<label ");
       {
-        final Option opt = itChk.next();
-        StringConcatenation _builder_1 = new StringConcatenation();
-        _builder_1.append("<input type=\"checkbox\" name=\"");
-        String _value = opt.getValue();
-        _builder_1.append(_value, "");
-        _builder_1.append("\" value=\"1\" />");
-        String _value_1 = opt.getValue();
-        _builder_1.append(_value_1, "");
-        _builder_1.append("<br>");
-        String _plus = (tmp + _builder_1);
-        tmp = _plus;
+        String _id = chk.getId();
+        boolean _notEquals = (!Objects.equal(_id, null));
+        if (_notEquals) {
+          _builder.append(" id=\"");
+          String _id_1 = chk.getId();
+          _builder.append(_id_1, "");
+        }
       }
-      boolean _hasNext_1 = itChk.hasNext();
-      _while = _hasNext_1;
+      _builder.append("\">");
+      _builder.newLineIfNotEmpty();
+      _builder.append("\t");
+      String _question = chk.getQuestion();
+      _builder.append(_question, "	");
+      _builder.newLineIfNotEmpty();
+      _builder.append("</label>");
+      _builder.newLine();
+      tmp = _builder.toString();
+      EList<Option> _options_1 = chk.getOptions();
+      final Iterator<Option> itChk = _options_1.iterator();
+      boolean _hasNext = itChk.hasNext();
+      boolean _while = _hasNext;
+      while (_while) {
+        {
+          final Option opt = itChk.next();
+          StringConcatenation _builder_1 = new StringConcatenation();
+          _builder_1.append("<input type=\"checkbox\" name=\"");
+          String _value = opt.getValue();
+          _builder_1.append(_value, "");
+          _builder_1.append("\" value=\"1\" />");
+          String _value_1 = opt.getValue();
+          _builder_1.append(_value_1, "");
+          _builder_1.append("<br>");
+          String _plus = (tmp + _builder_1);
+          tmp = _plus;
+        }
+        boolean _hasNext_1 = itChk.hasNext();
+        _while = _hasNext_1;
+      }
+    } else {
+      StringConcatenation _builder_1 = new StringConcatenation();
+      _builder_1.append("<input type=\"checkbox\" name=\"");
+      {
+        String _id_2 = chk.getId();
+        boolean _notEquals_1 = (!Objects.equal(_id_2, null));
+        if (_notEquals_1) {
+          String _id_3 = chk.getId();
+          _builder_1.append(_id_3, "");
+        } else {
+          String _question_1 = chk.getQuestion();
+          _builder_1.append(_question_1, "");
+        }
+      }
+      _builder_1.append("\" value=\"1\" />");
+      String _question_2 = chk.getQuestion();
+      _builder_1.append(_question_2, "");
+      tmp = _builder_1.toString();
     }
     return (tmp + "<br>");
   }
@@ -211,6 +281,185 @@ public class TransfoModelToText {
       boolean _hasNext_1 = itRd.hasNext();
       _while = _hasNext_1;
     }
+    return (tmp + "<br><br>");
+  }
+  
+  public String rendererDropDown(final DropDown dd) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("<label ");
+    {
+      String _id = dd.getId();
+      boolean _notEquals = (!Objects.equal(_id, null));
+      if (_notEquals) {
+        _builder.append(" id=\"");
+        String _id_1 = dd.getId();
+        _builder.append(_id_1, "");
+      }
+    }
+    _builder.append("\">");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    String _question = dd.getQuestion();
+    _builder.append(_question, "	");
+    _builder.newLineIfNotEmpty();
+    _builder.append("</label>");
+    _builder.newLine();
+    _builder.append("<select name=\"");
+    {
+      String _id_2 = dd.getId();
+      boolean _notEquals_1 = (!Objects.equal(_id_2, null));
+      if (_notEquals_1) {
+        String _id_3 = dd.getId();
+        _builder.append(_id_3, "");
+      } else {
+        String _question_1 = dd.getQuestion();
+        _builder.append(_question_1, "");
+      }
+    }
+    _builder.append("\">");
+    _builder.newLineIfNotEmpty();
+    String tmp = _builder.toString();
+    EList<Option> _options = dd.getOptions();
+    final Iterator<Option> itRd = _options.iterator();
+    int id = 0;
+    boolean _hasNext = itRd.hasNext();
+    boolean _while = _hasNext;
+    while (_while) {
+      {
+        final Option opt = itRd.next();
+        StringConcatenation _builder_1 = new StringConcatenation();
+        _builder_1.append("<option value=\"");
+        _builder_1.append(id, "");
+        _builder_1.append("\"/>");
+        String _value = opt.getValue();
+        _builder_1.append(_value, "");
+        _builder_1.append("</option>");
+        _builder_1.newLineIfNotEmpty();
+        String _plus = (tmp + _builder_1);
+        tmp = _plus;
+        int _plus_1 = (id + 1);
+        id = _plus_1;
+      }
+      boolean _hasNext_1 = itRd.hasNext();
+      _while = _hasNext_1;
+    }
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("</select>");
+    String _plus = (tmp + _builder_1);
+    tmp = _plus;
+    return (tmp + "<br><br>");
+  }
+  
+  public String rendererText(final Text txt) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("<label ");
+    {
+      String _id = txt.getId();
+      boolean _notEquals = (!Objects.equal(_id, null));
+      if (_notEquals) {
+        _builder.append(" id=\"");
+        String _id_1 = txt.getId();
+        _builder.append(_id_1, "");
+      }
+    }
+    _builder.append("\">");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    String _question = txt.getQuestion();
+    _builder.append(_question, "	");
+    _builder.newLineIfNotEmpty();
+    _builder.append("</label>");
+    _builder.newLine();
+    _builder.append("<input type=\"text\" name=\"");
+    {
+      String _id_2 = txt.getId();
+      boolean _notEquals_1 = (!Objects.equal(_id_2, null));
+      if (_notEquals_1) {
+        String _id_3 = txt.getId();
+        _builder.append(_id_3, "");
+      } else {
+        String _question_1 = txt.getQuestion();
+        _builder.append(_question_1, "");
+      }
+    }
+    _builder.append("\" />");
+    _builder.newLineIfNotEmpty();
+    String tmp = _builder.toString();
+    return (tmp + "<br><br>");
+  }
+  
+  public String rendererTextArea(final TextArea txt) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("<label ");
+    {
+      String _id = txt.getId();
+      boolean _notEquals = (!Objects.equal(_id, null));
+      if (_notEquals) {
+        _builder.append(" id=\"");
+        String _id_1 = txt.getId();
+        _builder.append(_id_1, "");
+      }
+    }
+    _builder.append("\">");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    String _question = txt.getQuestion();
+    _builder.append(_question, "	");
+    _builder.newLineIfNotEmpty();
+    _builder.append("</label>");
+    _builder.newLine();
+    _builder.append("<textarea rows=\"4\" cols=\"50\" name=\"");
+    {
+      String _id_2 = txt.getId();
+      boolean _notEquals_1 = (!Objects.equal(_id_2, null));
+      if (_notEquals_1) {
+        String _id_3 = txt.getId();
+        _builder.append(_id_3, "");
+      } else {
+        String _question_1 = txt.getQuestion();
+        _builder.append(_question_1, "");
+      }
+    }
+    _builder.append("\"></textarea>");
+    _builder.newLineIfNotEmpty();
+    String tmp = _builder.toString();
+    return (tmp + "<br><br>");
+  }
+  
+  public String parcoursSousQuestions(final EList<ElementUI> listeSousQuestion) {
+    String tmp = "";
+    Iterator<ElementUI> itL = listeSousQuestion.iterator();
+    boolean _hasNext = itL.hasNext();
+    boolean _while = _hasNext;
+    while (_while) {
+      ElementUI _next = itL.next();
+      String _switchElementUI = this.switchElementUI(_next);
+      String _plus = (tmp + _switchElementUI);
+      tmp = _plus;
+      boolean _hasNext_1 = itL.hasNext();
+      _while = _hasNext_1;
+    }
+    return tmp;
+  }
+  
+  public String rendererEnsembleQuestions(final EnsembleQuestions eq) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("<fieldset>");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("<legend>");
+    String _question = eq.getQuestion();
+    _builder.append(_question, "	");
+    _builder.append("</legend>");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t\t");
+    EList<ElementUI> _listeSousQuestion = eq.getListeSousQuestion();
+    String _parcoursSousQuestions = this.parcoursSousQuestions(_listeSousQuestion);
+    _builder.append(_parcoursSousQuestions, "		");
+    _builder.newLineIfNotEmpty();
+    _builder.append("</fieldset>");
+    _builder.newLine();
+    String tmp = _builder.toString();
     return (tmp + "<br><br>");
   }
   
